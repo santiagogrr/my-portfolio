55e83d8eca398b4f99713f7214cba39ba0faed7b
santiago.guerra <santiago.guerra@dual.hu>
2020-05-20 09:13:01 -0500

    Updates

---
+++ precache-manifest.556bb337afbadb3487f46a75b2880517.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c587148debddd6054e860b965464b3",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "d2448a5f3740b56719ba",
+    "url": "/my-portfolio/static/css/2.73c5f83b.chunk.css"
+  },
+  {
+    "revision": "a4960ca5b075257e944a",
+    "url": "/my-portfolio/static/css/main.85b86553.chunk.css"
+  },
+  {
+    "revision": "d2448a5f3740b56719ba",
+    "url": "/my-portfolio/static/js/2.e47da5cf.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my-portfolio/static/js/2.e47da5cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4960ca5b075257e944a",
+    "url": "/my-portfolio/static/js/main.e45692cb.chunk.js"
+  },
+  {
+    "revision": "5e43d190fa7f6fd53279",
+    "url": "/my-portfolio/static/js/runtime-main.8971698b.js"
+  },
+  {
+    "revision": "865e193452af48747be6fb896ec0a677",
+    "url": "/my-portfolio/static/media/New_CV.865e1934.pdf"
+  },
+  {
+    "revision": "658b2e105b68fa396a315a58d4624081",
+    "url": "/my-portfolio/static/media/Pshop_1139.658b2e10.png"
+  },
+  {
+    "revision": "a7845c97cc5cd4344ae8e55af8d71f84",
+    "url": "/my-portfolio/static/media/photo1.a7845c97.png"
+  }
+]);
